8e6cfd38aa295dddc0ce47cc68f4deec519691cd
Tishchenko_D <tishchenko.1993@list.ru>
2022-12-11 16:38:10 +0300

    Дз №6 первое

---
+++ task06/ConsolePersister.java
@@ -0,0 +1,12 @@
+
+public class ConsolePersister {
+    private User user;
+
+    public ConsolePersister(User user) {
+        this.user = user;
+    }
+
+    public void report() {
+        System.out.println("Report for user: " + user.getName());
+    }
+}
---
+++ task06/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		User user = new User("Bob");
+		Persister persister = new Persister(user);
+		ConsolePersister consolePersister = new ConsolePersister(user);
+		consolePersister.report();
+		;
+		persister.save();
+	}
+}
